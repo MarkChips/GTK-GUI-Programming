cdf396474a33311eccea967adf34b2bb48ecd39e
MarkChips <mark-chipperfield@outlook.com>
2025-12-18 15:05:54 +0000

    add checkbox to replace copy button, print value to console; build

--- gtktest.c
+++ gtktest.c
@@ -8,6 +8,12 @@ void end_program (GtkWidget *wid, gpointer ptr)
 	gtk_main_quit ();
 }
 
+void check_toggle (GtkWidget *wid, gpointer ptr)
+{
+	printf ("The state of the button is %d\n",
+		gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (wid)));
+}
+
 void copy_text (GtkWidget *wid, gpointer ptr)
 {
 	const char *input = gtk_entry_get_text (GTK_ENTRY (txt));
@@ -34,15 +40,17 @@ int main (int argc, char *argv[])
 		
 	GtkWidget *lbl = gtk_label_new ("My label");
 
-	GtkWidget *btn2 = gtk_button_new_with_label ("Copy button");
-	g_signal_connect (btn2, "clicked", G_CALLBACK (copy_text), lbl);
+	GtkWidget *chk = gtk_check_button_new_with_label ("My check");
+	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (chk), TRUE);
+	int state = gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (chk));
+	g_signal_connect (chk, "toggled", G_CALLBACK (check_toggle), NULL);
 
 	GtkAdjustment *adj = gtk_adjustment_new (0, -10, 10, 1, 0, 0);
 	txt = gtk_spin_button_new (adj, 0, 0);
 
 	GtkWidget *grd = gtk_grid_new ();
 	gtk_grid_attach (GTK_GRID (grd), lbl, 0, 0, 1, 1);
-	gtk_grid_attach (GTK_GRID (grd), btn2, 1, 0, 1, 1);
+	gtk_grid_attach (GTK_GRID (grd), chk, 1, 0, 1, 1);
 	gtk_grid_attach (GTK_GRID (grd), btn, 0, 1, 1, 1);
 	gtk_grid_attach (GTK_GRID (grd), txt, 1, 1, 1, 1);
 	gtk_container_add (GTK_CONTAINER (win), grd);
