97c7ff9728c6dfb10dabb8fb9c01c79e1e3df4de
MarkChips <mark-chipperfield@outlook.com>
2025-12-18 14:29:14 +0000

    add text field and copy button; build

--- gtktest.c
+++ gtktest.c
@@ -1,10 +1,17 @@
 #include <gtk/gtk.h>
 
 int count = 0;
+GtkWidget *txt;
 
 void end_program (GtkWidget *wid, gpointer ptr)
 {
 	gtk_main_quit ();
+}
+
+void copy_text (GtkWidget *wid, gpointer ptr)
+{
+	const char *input = gtk_entry_get_text (GTK_ENTRY (txt));
+	gtk_label_set_text (GTK_LABEL (ptr), input);
 }
 
 void count_button (GtkWidget *wid, gpointer ptr)
@@ -27,13 +34,16 @@ int main (int argc, char *argv[])
 		
 	GtkWidget *lbl = gtk_label_new ("My label");
 
-	GtkWidget *btn2 = gtk_button_new_with_label ("Count button");
-	g_signal_connect (btn2, "clicked", G_CALLBACK (count_button), lbl);
+	GtkWidget *btn2 = gtk_button_new_with_label ("Copy button");
+	g_signal_connect (btn2, "clicked", G_CALLBACK (copy_text), lbl);
+
+	txt = gtk_entry_new ();
 
 	GtkWidget *grd = gtk_grid_new ();
 	gtk_grid_attach (GTK_GRID (grd), lbl, 0, 0, 1, 1);
 	gtk_grid_attach (GTK_GRID (grd), btn2, 1, 0, 1, 1);
-	gtk_grid_attach (GTK_GRID (grd), btn, 0, 1, 2, 1);
+	gtk_grid_attach (GTK_GRID (grd), btn, 0, 1, 1, 1);
+	gtk_grid_attach (GTK_GRID (grd), txt, 1, 1, 1, 1);
 	gtk_container_add (GTK_CONTAINER (win), grd);
 	gtk_widget_show_all (win);
 	gtk_main ();
